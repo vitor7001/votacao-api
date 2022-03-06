353b2f465e2f8aee7a7e8f97db8288649d61d51f
Vitor Gomes <vitorgomes7001@gmail.com>
2022-03-05 23:02:56 -0300

    feat: adicionando bean para Rest template

--- src/main/java/com/votacao/votacaoapi/VotacaoApiApplication.java
+++ src/main/java/com/votacao/votacaoapi/VotacaoApiApplication.java
@@ -4,14 +4,19 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class VotacaoApiApplication {
 
 	@Bean
 	public ModelMapper modelMapper(){
-		ModelMapper modelMapper = new ModelMapper();
-		return modelMapper;
+		return new ModelMapper();
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return  new RestTemplate();
 	}
 
 	public static void main(String[] args) {
