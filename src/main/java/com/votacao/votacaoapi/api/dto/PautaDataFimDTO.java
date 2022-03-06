43e0305ddcbcaddbc45fa98b17f5fea6089a6b77
Vitor Gomes <vitorgomes7001@gmail.com>
2022-03-05 23:03:07 -0300

    feat: dto para receber a data fim de uma pauta

---
+++ src/main/java/com/votacao/votacaoapi/api/dto/PautaDataFimDTO.java
@@ -0,0 +1,15 @@
+package com.votacao.votacaoapi.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PautaDataFimDTO {
+    private String dataFim;
+
+}
